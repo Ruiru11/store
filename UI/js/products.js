f59db9e4cc68c40c1b5e49e70761339b71e6bb5e
mikotyson <mikeoyola@yahoo.com>
2018-10-31 17:55:54 +0300

    ft-[finishes #161585103] view all products

--- UI/js/products.js
+++ UI/js/products.js
@@ -1,19 +1,44 @@
-document.getElementById('getprod').addEventListener('submit', getprods);
-function getprods(){
-    fetch('https://njeri.herokuapp.com/api/v2/signup',{
+window.onload = function getprods(){
+    fetch('https://njeri.herokuapp.com/api/v2/products',{
         headers:{
             'Authorization': localStorage.getItem('token')
         },  
     })
-    .then(data=> res.json())
+    .then((res)=> res.json())
     .then(function (data) {
-        let output = `<h2>Products</h2>`;
-        data.forEach(function(products){
-        	output += `
-        	<div>
-        		<h3>${products.name}</h3>
-        	`;
+        console.log(data);
+        ul = document.getElementById('products');
+        let products = data.products;
+        return products.map(function(product){
+            let li = createNode('li'),
+                h1 = createNode('h1'),
+                h5 = createNode('h5'),
+                p  = createNode('p'),
+                h3 = createNode('h3'),
+                h4 = createNode('h4');
+            h1.innerHTML = `Product Name:${product.product_name}`;
+            h5.innerHTML = `Product Id:${product.product_id}`;
+            p.innerHTML = `Product Description:${product.description}`;
+            h3.innerHTML =`Product price:${product.price}`;
+            h4.innerHTML  =`Product Category:${product.category_name}`;
+            inject(li, h1);
+            inject(li, h5);
+            inject(li, p);
+            inject(li, h3);
+            inject(li, h4);
+            inject(ul, li);
         })
-        document.getElementById('get').innerHTML = output;  
+
     })
+    .catch(function(error){
+        console.log(error)
+    })
+}
+
+function createNode(el){
+    return document.createElement(el);
+}
+
+function inject(p, c){
+    return p.appendChild(c);
 }
